c65ebd8abe07466ace4d14f4ab8545d15c10ff2e
Ben Kenney <bkenney@gmail.com>
2026-01-03 10:44:54 -0500

    add tz detect for local timezone support

--- src/joplin_rag/urls.py
+++ src/joplin_rag/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('tz_detect/', include('tz_detect.urls')),
     path('', include('notes.urls')),
 ]
